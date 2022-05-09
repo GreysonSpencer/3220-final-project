4d71d04ce679dd00a1619f6a5b4701fd1c9821a0
Greyson Spencer <grsncd@mail.missouri.edu>
2022-05-09 16:03:00 -0500

    Add test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+#include "AES.h"
+#include "Encryptor.h"
+
 int main()
 {
-    
+    AES *aes = new AES();
+
+    Encryptor *enc = new Encryptor(aes);
+
+    enc->encrypt();
+
+    delete aes;
+    delete enc;
 }
