a9c2e441c5d5ab6801347f6f4e1a9454d6b2e522
Greyson Spencer <grsncd@mail.missouri.edu>
2022-05-09 14:57:12 -0500

    Add test includes

--- AES.h
+++ AES.h
@@ -2,6 +2,12 @@
 #define AES_H
 
 #include "SymmetricEnc.h"
+#include "cryptopp/cryptlib.h"
+#include "cryptopp/rijndael.h"
+#include "cryptopp/modes.h"
+#include "cryptopp/files.h"
+#include "cryptopp/osrng.h"
+#include "cryptopp/hex.h"
 
 class AES : public SymmetricEnc {
     public:
