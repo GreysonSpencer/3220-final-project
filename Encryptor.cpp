46b21d96615a39229cd167ca4140c9eafdde0b14
Greyson Spencer <grsncd@mail.missouri.edu>
2022-05-08 22:36:46 -0500

    Add shells

---
+++ Encryptor.cpp
@@ -0,0 +1,21 @@
+#include "Encryptor.h"
+
+Encryptor::Encryptor(EncryptionStrategy *strategy)
+{
+
+}
+
+Encryptor::~Encryptor()
+{
+
+}
+
+void Encryptor::changeStrategy(EncryptionStrategy *strategy)
+{
+
+}
+
+void Encryptor::encrypt()
+{
+
+}
