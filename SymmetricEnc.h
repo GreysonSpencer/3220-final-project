47e75fd112bfa5b403414a8ac0184c8f46181c46
Greyson Spencer <grsncd@mail.missouri.edu>
2022-05-08 22:26:00 -0500

    Add symmetric encryption subclass

--- SymmetricEnc.h
+++ SymmetricEnc.h
@@ -1,4 +1,12 @@
 #ifndef SYMMETRICENC_H
 #define SYMMETRICENC_H
 
+#include "EncryptionStrategy.h"
+
+class SymmetricEnc : public EncryptionStrategy {
+    public:
+        int publicKey;
+        ~SymmetricEnc(){};
+};
+
 #endif
