7270a61ac438cf5876c08e3114420e5838fd08ec
Greyson Spencer <grsncd@mail.missouri.edu>
2022-05-08 22:31:01 -0500

    Add encryptor client class

--- Encryptor.h
+++ Encryptor.h
@@ -1,4 +1,16 @@
 #ifndef ENCRYPTOR_H
 #define ENCRYPTOR_H
 
+#include "EncryptionStrategy.h"
+
+class Encryptor {
+    private:
+        EncryptionStrategy *_strategy;
+    public:
+        Encryptor(EncryptionStrategy *_strategy);
+        ~Encryptor();
+        void changeStrategy(EncryptionStrategy *_strategy);
+        void encrypt();
+};
+
 #endif
