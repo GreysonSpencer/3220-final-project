714cc2849a912a8b6adcc464f59c69fd0f49fa2f
Greyson Spencer <grsncd@mail.missouri.edu>
2022-05-08 22:27:00 -0500

    Add asymmetric encryption subclass

--- AsymmetricEnc.h
+++ AsymmetricEnc.h
@@ -1,4 +1,14 @@
 #ifndef ASYMMETRICENC_H
 #define ASYMMETRICENC_H
 
+#include "EncryptionStrategy.h"
+
+class AsymmetricEnc : public EncryptionStrategy {
+    public:
+        int publicKey;
+        ~AsymmetricEnc(){};
+    private:
+        int privateKey;
+};
+
 #endif
