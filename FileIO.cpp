4706c0e5720283ed6e74346949e96adcce672cb4
Greyson Spencer <grsncd@mail.missouri.edu>
2022-05-11 12:54:26 -0500

    Implement functions and constructor

--- FileIO.cpp
+++ FileIO.cpp
@@ -2,15 +2,15 @@
 
 FileIO::FileIO(std::string filename)
 {
-
+    _filename = filename;
 }
 
 void FileIO::setFilename(std::string filename)
 {
-
+    _filename = filename;
 }
 
 std::string FileIO::getFilename()
 {
-    
+    return _filename;
 }
