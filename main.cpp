1bea32d4b7f5b7bee72299a12d5fcb607723aee1
Greyson Spencer <grsncd@mail.missouri.edu>
2022-05-11 17:47:56 -0500

    Add decryption test

--- main.cpp
+++ main.cpp
@@ -2,14 +2,17 @@
 
 #include "AES.h"
 #include "Encryptor.h"
+#include "Decryptor.h"
 #include "FileIO.h"
 
 int main()
 {
+    // Encryption
     AES *aes = new AES();
     Encryptor *enc = new Encryptor(aes);
 
     enc->encrypt("test.txt");
+    delete enc;
 
     FileIO test1("test.txt");
     FileIO test2("test.txt.cipher");
@@ -19,5 +22,11 @@ int main()
     std::cout << test2.readFile() << std::endl;
     std::cout << test3.readFile() << std::endl;
 
-    delete enc;
+
+    // Decryption
+    AES *aesDec = new AES("test.txt.key");
+    Decryptor *dec = new Decryptor(aesDec);
+
+    dec->decrypt("test.txt.cipher");
+    delete dec;
 }
