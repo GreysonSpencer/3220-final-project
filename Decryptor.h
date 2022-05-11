8e260e68dcd7eb9b36ff4e2e890fd8db1a34d2ea
Greyson Spencer <grsncd@mail.missouri.edu>
2022-05-11 17:37:35 -0500

    Add decryptor class

--- Decryptor.h
+++ Decryptor.h
@@ -1,4 +1,17 @@
 #ifndef DECRYPTOR_H
 #define DECRYPTOR_H
 
+#include "EncryptionStrategy.h"
+#include <string>
+
+class Decryptor {
+    private:
+        EncryptionStrategy *_strategy;
+    public:
+        Decryptor(EncryptionStrategy *strategy);
+        ~Decryptor();
+        void changeStrategy(EncryptionStrategy *strategy);
+        void decrypt(std::string filename, std::string keyFilename);
+};
+
 #endif
