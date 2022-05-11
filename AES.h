4d6547ec0385ea418bc0cb7d8a63bd380dc995bb
Greyson Spencer <grsncd@mail.missouri.edu>
2022-05-10 22:15:44 -0500

    Test breaking includes

--- AES.h
+++ AES.h
@@ -2,9 +2,9 @@
 #define AES_H
 
 #include "SymmetricEnc.h"
-#include "cryptopp/cryptlib.h"
-#include "cryptopp/rijndael.h"
-#include "cryptopp/modes.h"
+#include "cryptlib.h"
+#include "rijndael.h"
+#include "modes.h"
 #include "cryptopp/files.h"
 #include "cryptopp/osrng.h"
 #include "cryptopp/hex.h"
