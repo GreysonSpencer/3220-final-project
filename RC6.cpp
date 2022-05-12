dff524ed518f99d1719bc8a194f4d33c46d1e01d
Greyson Spencer <grsncd@mail.missouri.edu>
2022-05-12 18:22:53 -0500

    Add random generation

--- RC6.cpp
+++ RC6.cpp
@@ -6,6 +6,10 @@ RC6::RC6()
 
     SecByteBlock keyBlock(CryptoPP::RC6::DEFAULT_KEYLENGTH);
     SecByteBlock ivBlock(CryptoPP::RC6::BLOCKSIZE);
+
+    AutoSeededRandomPool rng;
+    rng.GenerateBlock(keyBlock, CryptoPP::RC6::DEFAULT_KEYLENGTH);
+    rng.GenerateBlock(ivBlock, CryptoPP::RC6::BLOCKSIZE);
 }
 
 RC6::RC6(std::string keyFilename)
