0d68714cdd5ca47a5475e1233b101b08ed46e5db
Greyson Spencer <grsncd@mail.missouri.edu>
2022-05-12 20:54:08 -0500

    Add decryption cases

--- main.cpp
+++ main.cpp
@@ -162,6 +162,17 @@ int menu()
                         sleep(2);
                         break;
                     }
+
+                    case 2:
+                    {
+                        break;
+                    }
+
+                    case 3:
+                    break;
+
+                    default:
+                    break;
                 }
                 break;
             }
