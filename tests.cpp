678a2b6829a1ebe73fe1c0329bf455308e3d1c35
Greyson Spencer <grsncd@mail.missouri.edu>
2022-05-13 17:56:24 -0500

    Add second test

--- tests.cpp
+++ tests.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "AES.h"
+#include "RC6.h"
 #include "Encryptor.h"
 #include "Decryptor.h"
 #include <fstream>
@@ -26,6 +27,38 @@ TEST(AESEncryption, EncryptionDecryption)
     Decryptor *dec = new Decryptor(aesD);
 
     dec->decrypt("testText.txt.cipher");
+    delete dec;
+
+    std::string output;
+    std::ifstream outputstream("decoded_text.txt");
+
+    outputstream >> output;
+
+    EXPECT_EQ(testString, output);
+}
+
+TEST(RC6Encryption, EncryptionDecryption)
+{
+    std::string testString = "This is some input that I want to test.";
+    std::ofstream stream("testText2.txt");
+    if(!stream.is_open())
+    {
+        std::cout << "File failed to open.\n";
+        exit(EXIT_FAILURE);
+    }
+    stream << testString;
+    stream.close();
+
+    RC6 *rc6 = new RC6();
+    Encryptor *enc = new Encryptor(rc6);
+
+    enc->encrypt("testText2.txt");
+    delete enc;
+
+    RC6 *rc6D = new RC6("testText2.txt.key");
+    Decryptor *dec = new Decryptor(rc6D);
+
+    dec->decrypt("testText2.txt.cipher");
     delete dec;
 
     std::string output;
