ad7c4add94114aeed6f20ef48d10b133a98c6f87
Greyson Spencer <grsncd@mail.missouri.edu>
2022-05-12 21:02:10 -0500

    Remove test code

--- main.cpp
+++ main.cpp
@@ -16,29 +16,6 @@ int main(int argc, char* argv[])
     {
         std::cout << argc << argv[i] << std::endl;
     }
-    
-    // Encryption
-    // AES *aes = new AES();
-    // Encryptor *enc = new Encryptor(aes);
-
-    // enc->encrypt("test.txt");
-    // delete enc;
-
-    // FileIO test1("test.txt");
-    // FileIO test2("test.txt.cipher");
-    // FileIO test3("test.txt.key");
-
-    //std::cout << test1.readFile() << std::endl;
-    //std::cout << test2.readFile() << std::endl;
-    //std::cout << test3.readFile() << std::endl;
-
-
-    // Decryption
-    // AES *aesDec = new AES("test.txt.key");
-    // Decryptor *dec = new Decryptor(aesDec);
-
-    // dec->decrypt("test.txt.cipher");
-    // delete dec;
 
     if(argc < 2)
     {
