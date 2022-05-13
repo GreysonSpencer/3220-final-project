af4c2cf044e0ba7d1ef5faee062a3496eeab13e3
Greyson Spencer <grsncd@mail.missouri.edu>
2022-05-13 17:54:22 -0500

    Add first test

--- tests.cpp
+++ tests.cpp
@@ -6,7 +6,34 @@
 
 TEST(AESEncryption, EncryptionDecryption)
 {
-    
+    std::string testString = "This is some input that I want to test.";
+    std::ofstream stream("testText.txt");
+    if(!stream.is_open())
+    {
+        std::cout << "File failed to open.\n";
+        exit(EXIT_FAILURE);
+    }
+    stream << testString;
+    stream.close();
+
+    AES *aes = new AES();
+    Encryptor *enc = new Encryptor(aes);
+
+    enc->encrypt("testText.txt");
+    delete enc;
+
+    AES *aesD = new AES("testText.txt.key");
+    Decryptor *dec = new Decryptor(aesD);
+
+    dec->decrypt("testText.txt.cipher");
+    delete dec;
+
+    std::string output;
+    std::ifstream outputstream("decoded_text.txt");
+
+    outputstream >> output;
+
+    EXPECT_EQ(testString, output);
 }
 
 int main(int argc, char **argv) {
